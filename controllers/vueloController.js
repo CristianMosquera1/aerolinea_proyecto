213be7a2c7cc4fb10565aa8bcc2419541d6c186c
CristianMosquera1 <cristianmosqueraramirez@gmail.com>
2024-12-10 06:41:05 -0500

    Update vueloController.js

--- controllers/vueloController.js
+++ controllers/vueloController.js
@@ -1,20 +1,30 @@
-const Vuelo = require('../models/Vuelo');
+const Vuelo = require("../models/Vuelo");
 
-exports.getAllVuelos = async (req, res) => {
-    try {
-        const vuelos = await Vuelo.find();
-        res.json(vuelos);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+const getAllVuelos = async (req, res) => {
+  try {
+    const vuelos = await Vuelo.find();
+    res.status(200).json(vuelos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
-exports.createVuelo = async (req, res) => {
-    try {
-        const nuevoVuelo = new Vuelo(req.body);
-        const vueloGuardado = await nuevoVuelo.save();
-        res.json(vueloGuardado);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+const createVuelo = async (req, res) => {
+  const { fecha, destino, origen, matricula } = req.body;
+
+  try {
+    const newVuelo = new Vuelo({
+      fecha,
+      destino,
+      origen,
+      matricula,
+    });
+
+    await newVuelo.save();
+    res.status(201).json(newVuelo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
+
+module.exports = { getAllVuelos, createVuelo };
