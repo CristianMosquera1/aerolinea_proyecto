b3a914cd1f11277ac6b12984075318a5d08c11fd
CristianMosquera1 <cristianmosqueraramirez@gmail.com>
2024-12-10 06:42:48 -0500

    Update vueloRoutes.js

--- src/routes/vueloRoutes.js
+++ src/routes/vueloRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const {
-  getAllVuelos,
-  createVuelo,
-} = require("../../controllers/vueloController");
+const { getAllVuelos, createVuelo } = require("../controllers/vueloController");
 
 const router = express.Router();
 
+// Ruta para obtener todos los vuelos
 router.get("/", getAllVuelos);
+
+// Ruta para crear un nuevo vuelo
 router.post("/", createVuelo);
 
 module.exports = router;
